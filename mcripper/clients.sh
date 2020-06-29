06efde1e2974d13c0ea011acf63c5bff8995e165
scinorandex <scinorandex@gmail.com>
2020-06-29 23:08:33 +0800

    added clients.sh

---
+++ mcripper/clients.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+rm minecraftjsons 2>/dev/null
+
+#Get a list of urls from mojang
+echo "Curling version_manifest.json....."
+curl -s https://launchermeta.mojang.com/mc/game/version_manifest.json | jq -r '.versions | .[] | .url' >> minecraftjsons
+length=$(cat minecraftjsons | wc -l)
+
+#While loop to download everything
+for (( c=1; c<=length; c++ ))
+do
+   jsonurl=$(sed "${c}q;d" minecraftjsons)
+   jarurl=$(curl -s "${jsonurl}" | jq -r '.downloads.client.url')
+   jarver=$(curl -s "${jsonurl}" | jq -r '.id')
+   echo "Getting ${jarver} at ${jarurl}"
+   wget -nc -O "${jarver}.jar" "${jarurl}" 
+done
+
+rm minecraftjsons
